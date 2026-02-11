f91fde4dc3c906118d235ba7a3a39b0bb05d71d0
Rafael Schouten <rafaelschouten@gmail.com>
2026-02-11 14:03:55 +1100

    no theory tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,4 @@ using Aqua, SolarRadiation, DataFrames, CSV, Test, SafeTestsets, Unitful
     Aqua.test_all(SolarRadiation)
 end
 
-@safetestset "Theory-based tests" begin include("theory.jl") end
-
 @safetestset "Test against NicheMapR outputs" begin include("solar_radiation.jl") end
